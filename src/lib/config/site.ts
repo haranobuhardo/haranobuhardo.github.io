967e2649158c000ecc0f5a210d4c4b4f07ef04a9
haranobuhardo <haranobuhardo@gmail.com>
2024-10-19 23:37:36 +0700

    initial commit

---
+++ src/lib/config/site.ts
@@ -0,0 +1,17 @@
+import type { SiteConfig } from '$lib/types/site'
+
+export const site: SiteConfig = {
+  author: {
+    avatar: '/assets/logo@512.png',
+    bio: 'A simple person living in a complicated world. Sharing about Engineering, Tech, and Life',
+    name: 'Hardo',
+    status: '✍🏻',
+  },
+  description: 'Forked from SvelteKit/Urara',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  lang: 'en-US',
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  subtitle: 'Personal Blog',
+  themeColor: '#3D4451',
+  title: 'Hardo\'s Blog',
+}
